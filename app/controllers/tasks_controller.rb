300eded0b3ff1025c2fc5fbfc7b5d39fc2103860
Taylor Varoglu <tava8398@colorado.edu>
2021-06-25 11:29:20 -0600

    Add edit and delete to controller, routes, and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,13 +12,31 @@ class TasksController < ApplicationController
       title: params[:task][:title],
       description: params[:task][:description]
     })
-
     task.save
     redirect_to '/tasks'
   end
 
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+      title: params[:task][:title],
+      description: params[:task][:description]
+    })
+    task.save
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def delete
+    Task.destroy(params[:id])
+    redirect_to '/tasks'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id/edit/', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#delete'
 end
